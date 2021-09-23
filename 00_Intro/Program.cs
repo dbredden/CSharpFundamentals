5d8713f8e9c40301d7850f3489865ee3453323e9
Danny Redden <dannyredden18@gmail.com>
2021-09-23 17:51:49 -0400

    Add project files.

---
+++ 00_Intro/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _00_Intro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            // What is CSharp
+            // object oriented programming language: means our software is coded around objects
+            // POCO plain old c sharp CLR (common language runtime) object
+            // Properties make up an object and methods make up an object
+            // 4 Pillars of OOP
+            // 1. Encapsulation - Groups like variables in one object and reduces complexity. 
+                // Access modifiers
+            // 2. Abstraction
+                // coding lots of things behind one button
+            // 3. Inheritance - reduces redundant code
+            // 4. Polymorphism - allows to eliminate a lot of conditions. 
+        }
+    }
+}
---
+++ 02_ReferenceTypes/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("02_ReferenceTypes")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("02_ReferenceTypes")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("db16d6f4-3ed9-4258-b131-c46d954605d6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
