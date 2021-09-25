5fbbf8b42c1c3ef8e8e8bbec7c1694cba5e2d399
Danny Redden <dannyredden18@gmail.com>
2021-09-25 10:31:34 -0400

    conditional notes complete

---
+++ 04_IfElseStatements/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_IfElseStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hoursSpentStudying = 1;
+            if(hoursSpentStudying < 16)
+            {
+                Console.WriteLine("Student spent less than 16 hours studying");
+                Console.WriteLine("Another Line.");
+            }
+
+
+            bool choresAreDone = false;
+            if (choresAreDone)
+            {
+                Console.WriteLine("YEAAA! You are productive!");
+            }
+            else
+            {
+                Console.WriteLine("Get to work now!");
+            }
+
+            Console.WriteLine("How old are you?");
+            string ageInput = Console.ReadLine();
+            int age = Convert.ToInt32(ageInput);
+
+
+            if(age > 17)
+            {
+                Console.WriteLine("Congrats on being an adult!");
+            }
+            else
+            {
+                if (age < 6)
+                {
+                    Console.WriteLine("you are a kid");
+                }
+                else if (age < 0)
+                {
+                    Console.WriteLine("baby");
+                }
+                else
+                {
+                    Console.WriteLine("you are not born");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 04_SwitchCases/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_SwitchCases
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = 1;
+
+            switch (input)
+            {
+                case 1:
+                    Console.WriteLine("Hello World!");
+                    break;
+                case 2:
+                    Console.WriteLine("What are you doing?");
+                    break;
+                default:
+                    Console.WriteLine("This is default. It will be executed if no other case is evaluated");
+                    break;
+            }
+
+            DayOfWeek today = DateTime.Today.DayOfWeek;
+            switch(today)
+            {
+                case DayOfWeek.Monday:
+                    Console.WriteLine("Welcome to the beginning of a work week!");
+                    break;
+                case DayOfWeek.Tuesday:
+                case DayOfWeek.Wednesday:
+                case DayOfWeek.Thursday:
+                case DayOfWeek.Friday:
+                    Console.WriteLine("Still working!");
+                    break;
+                case DayOfWeek.Saturday:
+                case DayOfWeek.Sunday:
+                    Console.WriteLine("It's the weekend!!");
+                    break;
+            }
+
+            Console.WriteLine("How are you feeling today? On a scale from 1-5?");
+            string feeling = Console.ReadLine();
+            //int feelingInt = int.Parse(feeling);
+            //change string to int
+
+            switch (feeling) 
+            {
+                case "1":
+                case "0":
+                    Console.WriteLine("Why are you not feeling well?");
+                    break;
+                case "2":
+                    Console.WriteLine("How can you get to feeling better?");
+                    break;
+                case "3":
+                    Console.WriteLine("Sorry you aren't feeling better");
+                    break;
+                case "4":
+                    Console.WriteLine("Glad you are feeling good");
+                    break;
+                case "5":
+                    Console.WriteLine("Wow! you are feeling great!");
+                    break;
+                default:
+                    Console.WriteLine("That isn't between 1-5");
+                    break;
+
+            }
+ 
+
+            //Challenge
+            //ask user how they are feeling from scale 1-5 run through switch case and get a different statement for each level
+            // let user know if they are out of range
+
+            Console.ReadKey();
+        }
+    }
+}
